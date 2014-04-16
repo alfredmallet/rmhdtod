fccb7c951b53ca1e8fd4e41bb78bcb5efe72300a
amallet <amallet@3318479d-5d8a-4e76-975c-cdf357b99dbc>
2014-04-16 15:23:30 +0000

    Updates

--- grid.f90
+++ grid.f90
@@ -1,9 +1,64 @@
 module grid
 !***** Contains information about the grids in real and fourier space *****
 
-!temp until mpi is supported
+interface idx_local
+  module procedure idx_local_r, idx_local_k
+end interface
+
+interface proc_id
+  module procedure proc_id_r,proc_id_k
+end interface
+
+type :: r_layout_type
+   integer :: iproc
+   integer :: NLx, NLy, NLy_par
+   integer :: llim_world, ulim_world, llim_proc, ulim_proc
+end type r_layout_type
+
+type :: k_layout_type
+   integer :: iproc
+   integer :: NKx, NKy, NKx_par
+   integer :: llim_world, ulim_world, llim_proc, ulim_proc
+end type k_layout_type
+
+type (r_layout_type) :: r_variable
+type (k_layout_type) :: k_variable
 
 contains
+
+subroutine init_grid
+
+  use mp,only: iproc
+  use constants
+  implicit none
+  logical, save :: initialized = .false.
+
+  r_variable%iproc = iproc
+  r_variable%nlx = nlx
+  r_variable%nly = nly
+  r_variable%nly_par = nly_par
+  r_variable%llim_world = 1
+  r_variable%ulim_world = NLy
+  r_variable%llim_proc = 1
+  r_variable%ulim_proc = NLy_par
+
+  k_variable%iproc = iproc
+  k_variable%nkx = nkx
+  k_variable%nky = nky
+  k_variable%nkx_par = nkx_par
+  k_variable%llim_world = 1
+  k_variable%ulim_world = NKx
+  k_variable%llim_proc = 1
+  k_variable%ulim_proc = NKx_par
+
+end subroutine init_grid
+
+function proc_id_r(r_variable,j,zk)
+! returns proc no. which has j,zk for a realspace variable
+  use constants, only:nlz_par, NPE
+  implicit none
+  integer :: proc_id_r
+  type (r_layout_type),intent(in) :: r_variable
 
 real function xx(i)
 ! returns x value corresponding to i index in the real space grid
